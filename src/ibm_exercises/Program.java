9ffd081ad286738c46de2e324bb34a350ac052f1
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-27 17:24:34 -0300

    Add getFactorial

--- src/ibm_exercises/Program.java
+++ src/ibm_exercises/Program.java
@@ -1,9 +1,27 @@
 package ibm_exercises;
 
+import java.io.*;
+
 class Program {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    static long getFactorial(int n) {
+        if (n > 9) {
+            String stringNum = Integer.toString(n);
+            int size = stringNum.length();
+            int counter = stringNum.length();
+            int first = Integer.parseInt(stringNum.substring(0));
+            int last = Integer.parseInt(stringNum.substring(size - 1));
+            while (counter > 1) {
+                first = first * (counter - 1);
+                last = last * (counter - 1);
+                counter--;
+            }
+            return first + last;
+        }
+        return 0;
     }
 
+    public static void main(String[] args) throws IOException {
+        System.out.println(getFactorial(23));
+    }
 }
