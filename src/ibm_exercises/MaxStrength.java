a589cfc7f25c5f8c15bff9316a0415843fa4f069
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-29 00:10:53 -0300

    Implements maxStrength getter

---
+++ src/ibm_exercises/MaxStrength.java
@@ -0,0 +1,59 @@
+package ibm_exercises;
+
+import java.util.*;
+
+public class MaxStrength {
+    private static int getFactorial(int n) {
+        int i = n - 1;
+        while (i > 1) {
+            n *= i;
+            i--;
+        }
+        return n;
+    }
+
+    private static int findFactorial(int n) {
+        if (n <= 9) {
+            n = getFactorial(n);
+            return n;
+        } else {
+            String text = Integer.toString(n);
+            String[] numArr = text.split("", 0);
+            int res = 0;
+            for (String item : numArr) {
+                int num = Integer.parseInt(item);
+                if (num != 0) {
+                    num = getFactorial(num);
+                    res += num;
+                } else {
+                    res += 1;
+                }
+            }
+            return res;
+        }
+    }
+
+    private static List<Integer> mountTeam(int n) {
+        ArrayList<Integer> team = new ArrayList<Integer>(Arrays.asList(n));
+        for (int i = 1; i < 10; i++) {
+            team.add(findFactorial(team.get(i - 1)));
+        }
+        return team;
+    }
+
+    private static int findLeader(List<Integer> list) {
+        int leader = -1;
+        for (int item : list) {
+            if (item > leader) {
+                leader = item;
+            }
+        }
+        return leader;
+    }
+
+    static long getMaxStrength(int initialValue) {
+        List<Integer> team = mountTeam(initialValue);
+        int leader = findLeader(team);
+        return leader * team.size();
+    }
+}
--- src/ibm_exercises/Program.java
+++ src/ibm_exercises/Program.java
@@ -1,29 +1,8 @@
 package ibm_exercises;
 
 import java.io.*;
-import java.util.*;
 
 class Program {
-
-    static int getFactorial(int n) {
-        int i = n - 1;
-        while (i > 1) {
-            n *= i;
-            i--;
-        }
-        System.out.println("FACTORIAL " + n);
-        return n;
-    }
-
-    static List<Integer> mountTeam(int initialValue) {
-        ArrayList<Integer> team = new ArrayList<Integer>(Arrays.asList(initialValue));
-        for (int i = 0; i <= 4; i++) {
-            team.add(getFactorial(team.get(i)));
-            System.out.println(team);
-
-        }
-        return team;
-    }
 
     /*
      * Dado um numero inicial, calcular "times" com base na seguinte regra
@@ -35,6 +14,6 @@ class Program {
      */
 
     public static void main(String[] args) throws IOException {
-        System.out.println(mountTeam(5));
+        System.out.println("MAX STRENGTH - " + MaxStrength.getMaxStrength(5));
     }
 }
